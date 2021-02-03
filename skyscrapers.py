886bf01c0b5dbf6b1b4b6b398a519d8a6a3f8552
vityha <dpyg.ttpocto@gmail.com>
2021-02-03 09:55:27 +0200

    Added function check_not_finished_board.

--- skyscrapers.py
+++ skyscrapers.py
@@ -37,20 +37,23 @@ def left_to_right_check(input_line: str, pivot: int):
     return True
 
 
-# def check_not_finished_board(board: list):
-#     """
-#     Check if skyscraper board is not finished, i.e., '?' present on the game board.
+def check_not_finished_board(board: list):
+    """
+    Check if skyscraper board is not finished, i.e., '?' present on the game board.
 
-#     Return True if finished, False otherwise.
+    Return True if finished, False otherwise.
 
-#     >>> check_not_finished_board(['***21**', '4?????*', '4?????*', '*?????5', '*?????*', '*?????*', '*2*1***'])
-#     False
-#     >>> check_not_finished_board(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
-#     True
-#     >>> check_not_finished_board(['***21**', '412453*', '423145*', '*5?3215', '*35214*', '*41532*', '*2*1***'])
-#     False
-#     """
-#     for row in 
+    >>> check_not_finished_board(['***21**', '4?????*', '4?????*', '*?????5', '*?????*', '*?????*', '*2*1***'])
+    False
+    >>> check_not_finished_board(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    True
+    >>> check_not_finished_board(['***21**', '412453*', '423145*', '*5?3215', '*35214*', '*41532*', '*2*1***'])
+    False
+    """
+    for row in board:
+        if "?" in row:
+            return False
+    return True
 
 
 # def check_uniqueness_in_rows(board: list):
